ebd216d7f50f2c8b7142d0e69130fb4b5e994ec5
Rodkross <pretorsouza@gmail.com>
2024-07-31 14:35:26 -0300

    concluindo a fase 3

--- lib/tcc.dart
+++ lib/tcc.dart
@@ -1,7 +1,13 @@
-void main() {
-  String endrecoCompleto = 'Rua Hiroaki Torigoi, 182 - Bangu / RJ';
+import 'dart:io';
 
-  print('Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento');
-  print('Estamos localizados na $endrecoCompleto. Venha nos visitar!');
+void main() {
+  //String endrecoCompleto = 'Rua Hiroaki Torigoi, 182 - Bangu / RJ';
+
+  print('Digite seu nome:');
+  String nomeCliente = stdin.readLineSync() as String;
+
+  print('"Prezado(a), $nomeCliente. Seja muito bem-vindo(a) à nossa loja."');
+  print('Oferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja.');
+  //print('Estamos localizados na $endrecoCompleto. Venha nos visitar!');
 
 }
