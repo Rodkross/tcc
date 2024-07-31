9b3d373bd63acf5efb306acbaff87fc83c7a0173
Rodkross <pretorsouza@gmail.com>
2024-07-31 14:48:26 -0300

    conclusao da fase 4

--- lib/tcc.dart
+++ lib/tcc.dart
@@ -7,7 +7,22 @@ void main() {
   String nomeCliente = stdin.readLineSync() as String;
 
   print('"Prezado(a), $nomeCliente. Seja muito bem-vindo(a) à nossa loja."');
-  print('Oferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja.');
+  //print('Oferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja.');
   //print('Estamos localizados na $endrecoCompleto. Venha nos visitar!');
+  
+  print('1 - Ver ofertas de Produtos.');
+  print('2 - Ver ofertas de Serviços.');
+  var menuLeitura = stdin.readLineSync() as String;
+  int menu = int.parse(menuLeitura);
+
+  switch(menu){
+    case 1: print('Ração Royal Canin Indor 7,5kg com o valor promocional de R\$ 280,00');
+      break;
+    case 2: print('Banho e tosa na promoção pelo preço do banho R\$ 54,00');
+      break;
+    default:
+      print('Opção inválida.');  
+  }
+
 
 }
