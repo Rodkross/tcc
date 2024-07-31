87210154e4fc92120c268fb905dd6fa28a096b46
Rodkross <pretorsouza@gmail.com>
2024-07-31 14:28:49 -0300

    implementando fase 2

--- lib/tcc.dart
+++ lib/tcc.dart
@@ -1,5 +1,7 @@
 void main() {
-  print('Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento');
+  String endrecoCompleto = 'Rua Hiroaki Torigoi, 182 - Bangu / RJ';
 
+  print('Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento');
+  print('Estamos localizados na $endrecoCompleto. Venha nos visitar!');
 
 }
